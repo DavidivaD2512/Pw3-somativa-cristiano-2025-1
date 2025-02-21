20999644f44304d789e2bcaa09835e0302308f94
David Ramos <80305539+DavidivaD2512@users.noreply.github.com>
2025-02-20 22:37:06 -0300

    Começo da NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
 import './App.css'
 
-import BookCard from './components/BookCard'
-
-import capa_livro from './assets/cavernas_aco.jpg'
+import NavBar from './components/layout/NavBar.jsx'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>PW3 - WEBAPP - LIVRARIA</h1>
-        <BookCard 
-          titulo='As Cavernas de Aço' 
-          autor='Isaac Azimov'
-          imagem={capa_livro} />
-        
-        {/* <BookCard 
-          titulo='O Sol Desvelado' 
-          autor='Isaac Azimov' />
-
-        <BookCard 
-          titulo='O Fim da Infância' 
-          autor='Arthur C. Clark' />
-
-        <BookCard 
-          titulo='Neuromancer' 
-          autor='William Gibson' /> */}
-
+        <NavBar/>
       </div>
       
 
