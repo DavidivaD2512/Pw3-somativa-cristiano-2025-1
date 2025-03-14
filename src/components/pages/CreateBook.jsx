63883e34ad8105038e9c3309803559ca5c7ca6db
DavidivaD2512 <daviddivad25.12@gmail.com>
2025-03-13 21:23:10 -0300

    Criação das Handlers

--- src/components/pages/CreateBook.jsx
+++ src/components/pages/CreateBook.jsx
@@ -1,50 +1,76 @@
 import React from 'react';
+import { useState } from 'react';
 import style from './CreateBook.module.css'
 
-import Input from '../form/input.jsx'
-import Select from '../form/Select.jsx'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import Button from '../form/Button'
 
 const CreateBook = ()=>{
+
+    const [book, setBook] = useState({})
+
+    function handlerChangeBook(event) {
+        setBook({...book, [event.target.name] : event.target.value})
+    }
+
+    function handlerChangeCategory(event) {
+        setBook({...book, category : event.target.options[event.target.selectedIndex].text}) 
+    }
+
+    function submit(event) {
+        event.preventDefault();
+        console.log(book)
+    }
+
     return(
 
         <section className={style.create_book_container}>
 
             <h1>CADASTRO DE LIVRO</h1>
 
-            <Input 
-                text='Nome do livro'
-                type='text'
-                name='txt_livro'
-                id='txt_livro'
-                placeholder='Digite o nome do livro'
-             />
+            <form onSubmit={submit}>
 
-            <Input 
-                text='Autor do livro'
-                type='text'
-                name='txt_autor'
-                id='txt_autor'
-                placeholder='Digite o nome do autor livro'
-             />
+                <Input 
+                    text='Nome do livro'
+                    type='text'
+                    name='txt_livro'
+                    id='txt_livro'
+                    placeholder='Digite o nome do livro'
+                    handlerChange={handlerChangeBook}
+                />
+
+                <Input 
+                    text='Autor do livro'
+                    type='text'
+                    name='txt_autor'
+                    id='txt_autor'
+                    placeholder='Digite o nome do autor livro'
+                    handlerChange={handlerChangeBook}
+                />
 
 
-            <Input 
-                text='Descrição do livro'
-                type='text'
-                name='txt_descricao'
-                id='txt_descricao'
-                placeholder='Digite a descrição do livro'
-             />
+                <Input 
+                    text='Descrição do livro'
+                    type='text'
+                    name='txt_descricao'
+                    id='txt_descricao'
+                    placeholder='Digite a descrição do livro'
+                    handlerChange={handlerChangeBook}
+                />
 
-             <Select 
-                name='slc_categoria' 
-                id='slc_categoria'
-                text='Categoria do livro' 
-            />
+                <Select 
+                    name='slc_categoria' 
+                    id='slc_categoria'
+                    text='Categoria do livro' 
+                    handlerChange={handlerChangeCategory}
+                />
 
-            <Button
-                label='CADASTRAR LIVRO'
-            />
+                <Button
+                    label='CADASTRAR LIVRO'
+                />
+
+            </form>
 
 
         </section>
